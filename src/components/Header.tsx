153084361a185aa08e32f4cd97e825b7d72d75a4
0xjojikun <jojikun@intuition.systems>
2023-09-12 17:50:16 -0600

    Add authentication
    * Add next-auth + auth + authguard
    * Add apikey validation endpoints
    * Add didSession auth + validation
    * Add authentication UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,25 @@
 import Link from 'next/link'
 import { AccountButton } from './AccountButton'
 import IntuitionLogotype from './svg/intuition-logotype'
+import AuthenticateCard from '@/app/authenticate-card'
+import { useAccount } from 'wagmi'
 
 export default function Header() {
+  const { isConnected } = useAccount()
   return (
-    <div className="max-w-7xl w-full items-center justify-between lg:flex">
-      <div className="space-y-8 max-lg:flex max-lg:flex-col max-lg:items-center max-lg:mt-10">
-        <Link href="/" className="max-lg:mb-8">
-          <IntuitionLogotype className="max-lg:h-14" />
+    <div className="max-w-7xl w-full items-start justify-between lg:flex">
+      <div className="space-y-6 max-lg:flex max-lg:flex-col max-lg:items-center">
+        <Link href="/">
+          <IntuitionLogotype />
         </Link>
-        <p className="rounded-md px-3 py-2 font-mono text-sm cursor-default border border-stone-700/50 bg-gray-50/5 backdrop-blur-sm max-lg:text-center">
+        <p className="rounded-md px-3 py-2 font-mono text-sm cursor-default border border-stone-400/50 bg-gray-50/5 backdrop-blur-sm">
           Get started by editing{' '}
           <span className="font-bold text-success-500">src/app/page.tsx</span>
         </p>
       </div>
-      <div className="flex flex-row items-center mt-4 max-lg:justify-center max-lg:py-4">
+      <div className="flex flex-col items-end max-lg:items-center mt-4 max-lg:justify-center gap-4">
         <AccountButton />
+        {isConnected && <AuthenticateCard />}
       </div>
     </div>
   )
